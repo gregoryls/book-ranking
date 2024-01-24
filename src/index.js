51bb445f50d89956124c6aea02f222909f5ae5eb
Greg Schneider <greg53x@msn.com>
2024-01-23 23:43:53 -0700

    simple loop to filter out unread books

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import bookList from "./bookList.json";
 
-console.log(bookList);
+console.log(bookList[0]);
+const readBooks = [];
+
+for (let i = 0; i < bookList.length; i += 1) {
+  if (bookList[i]["Date Read"]) {
+    readBooks.push(bookList[i]);
+  }
+  //   console.log(bookList[i]["Date Read"]);
+}
+
+console.log(readBooks);
