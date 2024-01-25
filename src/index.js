0a6b018cd29efea470044b4632d35a0172d39b43
Greg Schneider <greg53x@msn.com>
2024-01-24 22:09:46 -0700

    split read books into arrays for each rating bucket

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import bookList from "./bookList.json";
 
 console.log(bookList[0]);
 const readBooks = [];
+const rating1Books = [];
+const rating2Books = [];
+const rating3Books = [];
+const rating4Books = [];
+const rating5Books = [];
 
 for (let i = 0; i < bookList.length; i += 1) {
   if (bookList[i]["Date Read"]) {
     readBooks.push(bookList[i]);
   }
-  //   console.log(bookList[i]["Date Read"]);
+  if (bookList[i]["My Rating"] === 1) {
+    rating1Books.push(bookList[i]);
+  }
+  if (bookList[i]["My Rating"] === 2) {
+    rating2Books.push(bookList[i]);
+  }
+  if (bookList[i]["My Rating"] === 3) {
+    rating3Books.push(bookList[i]);
+  }
+  if (bookList[i]["My Rating"] === 4) {
+    rating4Books.push(bookList[i]);
+  }
+  if (bookList[i]["My Rating"] === 5) {
+    rating5Books.push(bookList[i]);
+  }
 }
 
 console.log(readBooks);
+console.log(rating1Books);
