58840879810e79f19a82d9e50a30d6e2409af239
Greg Schneider <greg53x@msn.com>
2024-01-25 22:30:46 -0700

    webpack entry for utils.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
+    utils: "./src/utils.js",
     // module1: "./src/module1.js",
   },
   devtool: "inline-source-map",
