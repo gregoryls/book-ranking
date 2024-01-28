b0d6e062806ac9107527b5487c823e2ed519b2c9
Greg Schneider <greg53x@msn.com>
2024-01-27 21:57:28 -0700

    add numbering to book list generation

--- src/utils.js
+++ src/utils.js
@@ -3,10 +3,12 @@ export function renderBookList(bookList) {
   for (let i = 0; i < bookList.length; i += 1) {
     const div = document.createElement("div");
     const p = document.createElement("p");
+    const h3 = document.createElement("h3");
 
     div.classList.add("bookListDisplay");
+    h3.textContent = i;
     p.textContent = bookList[i].Title;
-    div.append(p);
+    div.append(h3, p);
     bookWrap.append(div);
   }
 }
