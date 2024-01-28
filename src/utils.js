733f1ce747d97715d27ef51f0aedbc50e21dd777
Greg Schneider <greg53x@msn.com>
2024-01-27 21:58:43 -0700

    add author to book list generation

--- src/utils.js
+++ src/utils.js
@@ -3,12 +3,14 @@ export function renderBookList(bookList) {
   for (let i = 0; i < bookList.length; i += 1) {
     const div = document.createElement("div");
     const p = document.createElement("p");
+    const authorP = document.createElement("p");
     const h3 = document.createElement("h3");
 
     div.classList.add("bookListDisplay");
     h3.textContent = i;
     p.textContent = bookList[i].Title;
-    div.append(h3, p);
+    authorP.textContent = bookList[i].Author;
+    div.append(h3, p, authorP);
     bookWrap.append(div);
   }
 }
