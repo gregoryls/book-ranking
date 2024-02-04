8b9206660380b9057ffd55fb36c7acb71cbeb2eb
Greg Schneider <greg53x@msn.com>
2024-02-03 22:19:26 -0700

    move appropriate functions from utils module to newBook

--- src/utils.js
+++ src/utils.js
@@ -41,35 +41,6 @@ export function renderBookList(bookList) {
   }
 }
 
-export function getGoodreadsIDInput() {
-  const ID = document.getElementById("goodreadsID");
-  return ID.value;
-}
-export function getAuthorInput() {
-  const author = document.getElementById("author");
-  return author.value;
-}
-
-export function getAdditionalAuthors() {
-  const authors = document.getElementById("additionalAuthors");
-  const authorsArray = authors.value.split(",");
-  return authorsArray;
-}
-
-export function getISBN() {
-  const ISBN = document.getElementById("ISBN");
-  return ISBN.value;
-}
-
-export function getStarRating() {
-  const rating = document.getElementById("starRating");
-  return rating.value;
-}
-
-export function getPublicRating() {
-  const rating = document.getElementById("publicRating");
-  return rating.value;
-}
 // export function getNewBookInputs() {
 //   const goodreadsID = document.getElementById("goodreadsID").value;
 //   console.log(goodreadsID);
