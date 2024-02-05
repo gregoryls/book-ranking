905e284f8d0585c61b6792877a3547680e3ec729
Greg Schneider <greg53x@msn.com>
2024-02-04 23:23:11 -0700

    get functions for remaining new book inputs

--- src/newBook.js
+++ src/newBook.js
@@ -27,3 +27,60 @@ export function getPublicRating() {
   const rating = document.getElementById("publicRating");
   return rating.value;
 }
+
+export function getPublisher() {
+  const publisher = document.getElementById("publisher");
+  return publisher.value;
+}
+
+export function getYearPublished() {
+  const year = document.getElementById("yearPublished");
+  return year.value;
+}
+
+export function getPages() {
+  const pages = document.getElementById("pages");
+  return pages.value;
+}
+
+export function getOriginalPublicationYear() {
+  const year = document.getElementById("originalPublicationyear");
+  return year.value;
+}
+
+export function getStartDate() {
+  const date = document.getElementById("dateStarted");
+  return date.value;
+}
+
+export function getFinishDate() {
+  const date = document.getElementById("dateFinished");
+  return date.value;
+}
+
+export function getAddedDate() {
+  const date = document.getElementById("dateAdded");
+  return date.value;
+}
+
+export function getFormat() {
+  const format = document.getElementById("format");
+  return format.value;
+}
+
+export function getReread() {
+  const reread = document.getElementById("reread");
+  if (reread.checked) return true;
+  return false;
+}
+
+export function getGenre() {
+  const genreTags = document.getElementById("genreTags");
+  const genreArray = genreTags.value.split(",");
+  return genreArray;
+}
+
+export function getReview() {
+  const review = document.getElementById("review");
+  return review.value;
+}
