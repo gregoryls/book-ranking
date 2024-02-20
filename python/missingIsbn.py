247ca6884070d53cbeebd02815f9f4ed40a87515
Greg Schneider <greg53x@msn.com>
2024-02-19 23:38:57 -0700

    python script uses isbnlib to find isbns from title and author string

--- python/missingIsbn.py
+++ python/missingIsbn.py
@@ -1,1 +1,34 @@
-print('test')
+import json
+from isbnlib import meta, isbn_from_words
+
+# remove isbntools, just need isbnlib
+
+def lookup_isbn(title, author):
+    # Load bookList from JSON file
+    with open('src/bookList.json', 'r', encoding='utf-8') as file:
+        book_list = json.load(file)
+
+    for book in book_list:
+
+
+        if book.get('ISBN13') == None:
+           
+           title_author_str = f"{book.get('Title')} {book.get('Author')}"
+           isbn = isbn_from_words(title_author_str)
+           print(isbn)
+            
+        #     if isbn:
+        #         # If ISBN is already available in the bookList, print it
+        #         print(f"ISBN for '{title}' by {author}: {isbn}")
+        #     else:
+        #         # If ISBN is not available, try looking it up using isbnlib
+        #         isbn = meta(title, author)['ISBN-13']
+        #         print(f"Lookup ISBN for '{title}' by {author}: {isbn}")
+        #         book['isbn'] = isbn  # Update bookList with the ISBN
+
+    # Update bookList JSON file with added ISBNs
+    # with open('bookList.json', 'w') as file:
+    #    json.dump(book_list, file, indent=2)
+
+# Example usage
+lookup_isbn("Arboreality", "Rebecca Campbell")
