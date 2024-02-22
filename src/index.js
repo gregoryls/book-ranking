51eeb5756ca8d41c087e5ba093344023ce0cdbdb
Greg Schneider <greg53x@msn.com>
2024-02-21 23:01:28 -0700

    remove unused puppeteer-based isbn scraping module

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import * as utils from "./utils";
 // scrape images - 'img.ResponsiveImage' should hit covers
 // 22604815, isbn doesn't exist
 // if statement for null isbn image names
-// https://pypi.org/project/isbntools/
+// update python script notes
 
 console.log(bookList[0]);
 const readBooks = [];
