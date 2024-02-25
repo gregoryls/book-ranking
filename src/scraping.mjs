cac0a47fe023b0529141c76a4b3d69d80cc20cc3
Greg Schneider <greg53x@msn.com>
2024-02-24 23:53:41 -0700

    delete trial code from scraping module

--- src/scraping.mjs
+++ src/scraping.mjs
@@ -1,55 +1,6 @@
-// import axios from "axios";
-// import cheerio from "cheerio";
 import puppeteer from "puppeteer";
 import fs from "fs";
 import bookList from "./bookList.json" with { type: "json" };
-
-// export async function scrapePageCheerio(bookID) {
-//   const url = `https://www.goodreads.com/book/show/${bookID}`;
-
-//   try {
-//     // Fetch the HTML content of the page
-//     const response = await axios.get(url);
-//     const html = response.data;
-
-//     // Load the HTML content into Cheerio
-//     const $ = cheerio.load(html);
-
-//     // Extract images
-//     const images = [];
-//     $("img.ResponsiveImage").each((index, element) => {
-//       const imageUrl = $(element).attr("src");
-//       images.push(imageUrl);
-//     });
-
-//     // array of image URLs for the current page
-//     console.log(`Images on page ${bookID}:`, images);
-//   } catch (error) {
-//     console.error(`Error fetching or parsing page ${bookID}:`, error.message);
-//   }
-// }
-
-// const url = "https://www.goodreads.com/book/show/45892276";
-
-// const main = async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto(url);
-
-//   const images = await page.evaluate(() => {
-//     const img = document.querySelector("img.ResponsiveImage");
-//     return img.src;
-//   });
-//   console.log(images);
-//   console.log("test");
-
-//   // await page.screenshot({ path: "screenshot.png" });
-//   await browser.close();
-// };
-
-// main();
-
-// const url = "https://www.goodreads.com/book/show/45892276";
 
 const IdToISBN = {};
 for (let i = 0; i < bookList.length; i += 1) {
