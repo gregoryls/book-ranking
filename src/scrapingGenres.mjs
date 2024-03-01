c9a075016a9ed1f9346e6077702a1baaba437718
Greg Schneider <greg53x@msn.com>
2024-03-01 00:03:19 -0700

    update genre script to utilize try catch error handling and page reload on errors

--- src/scrapingGenres.mjs
+++ src/scrapingGenres.mjs
@@ -2,35 +2,52 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 import bookList from "./bookList.json" with { type: "json" };
 
-// let genreElements;
+const maxRetries = 3;
 
 const main = async () => {
   for (let i = 0; i < 2; i += 1) {
     const bookID = bookList[i]["Book Id"];
     const url = "https://www.goodreads.com/book/show/" + bookID;
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+    let retries = 0;
+    let genreElements;
 
-    await page.waitForNetworkIdle();
+    while (retries < maxRetries) {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-    const genreElements = await page.$$eval(
-      ".BookPageMetadataSection__genreButton a span",
-      (elements) => elements.map((element) => element.textContent),
-    );
+      try {
+        await page.goto(url);
+        await page.waitForNetworkIdle();
 
-    // console.log(genreElements);
+        genreElements = await page.$$eval(
+          ".BookPageMetadataSection__genreButton a span",
+          (elements) => elements.map((element) => element.textContent),
+        );
+
+        // If successful, break out of the loop
+        break;
+      } catch (error) {
+        console.error(
+          `Error fetching data for bookID ${bookID}: ${error.message}`,
+        );
+      } finally {
+        await page.close();
+        await browser.close();
+      }
+
+      // Increment retries and wait for a short time before the next attempt
+      retries += 1;
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+
+    // Update bookList with genreTags
     bookList[i].genreTags = genreElements;
-    // console.log(bookList[i]);
 
-    //   await fs.promises.writeFile(
-    //     `src/scrapeCovers/${IdToISBN[bookID]}.jpg`,
-    //     imageBuffer,
-    //   );
     await page.close();
     await browser.close();
   }
+
   const updatedBookList = JSON.stringify(bookList, null, 2);
   await fs.promises.writeFile("src/genreTest.json", updatedBookList);
 };
