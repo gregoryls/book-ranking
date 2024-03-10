87e129d80e1713cf10deb566d7ee95d7297a3242
Greg Schneider <greg53x@msn.com>
2024-03-09 21:43:24 -0700

    update bookList genres 500-599

--- src/scrapingGenres.mjs
+++ src/scrapingGenres.mjs
@@ -5,7 +5,7 @@ import bookList from "./bookList.json" with { type: "json" };
 const maxRetries = 3;
 
 const main = async () => {
-  for (let i = 400; i < 500; i += 1) {
+  for (let i = 500; i < 600; i += 1) {
     const bookID = bookList[i]["Book Id"];
     const url = "https://www.goodreads.com/book/show/" + bookID;
 
