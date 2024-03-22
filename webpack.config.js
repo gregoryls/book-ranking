e7c7539f80bc2b9cfb1b700bb99592049f7ddaba
Greg Schneider <greg53x@msn.com>
2024-03-21 22:57:31 -0600

    update webpack config to sort image assets into dist/img

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "img/[hash][ext][query]",
     clean: true,
   },
   plugins: [
