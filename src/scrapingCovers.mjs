9467969169959e3732764009f26061d21fb188f7
Greg Schneider <greg53x@msn.com>
2024-03-26 21:49:30 -0600

    covers 1450-1712

--- src/scrapingCovers.mjs
+++ src/scrapingCovers.mjs
@@ -21,7 +21,7 @@ const main = async () => {
     // browser outside for loop to reuse same instance
     browser = await puppeteer.launch();
 
-    for (let i = 1250; i < 1450; i += 1) {
+    for (let i = 1450; i < 1713; i += 1) {
       let page, pageNew;
       let retries = 0;
       const bookID = bookList[i]["Book Id"];
