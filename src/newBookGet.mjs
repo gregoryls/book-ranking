2c36390d2a817d8a3e7fc34005d30e6708001f16
Greg Schneider <greg53x@msn.com>
2025-05-01 22:45:08 -0600

    new book puppeteer page load

--- src/newBookGet.mjs
+++ src/newBookGet.mjs
@@ -2,4 +2,25 @@ import fs from "fs";
 import puppeteer from "puppeteer";
 import bookList from "./bookList.json" with { type: "json" };
 
-const main = async () => {};
+maxRetries = 3;
+
+const main = async () => {
+    let browser;
+
+
+        browser = await puppeteer.launch();
+        let retries = 0;
+        // consider const page
+        let page;
+        
+        const bookID = 36578942
+
+        while (retries < maxRetries) {
+            try {
+                const url = "https://www.goodreads.com/book/show/" + bookID;
+                page = await browser.newPage();
+                await page.goto(url);
+            }
+        }
+ 
+};
