bf610b6fef0ac149ef6a57aaf7107adee59caea0
Greg Schneider <greg53x@msn.com>
2025-05-22 22:43:15 -0600

    splice out audiobook genre tags in loop

--- src/removeGenreTag.js
+++ src/removeGenreTag.js
@@ -1,3 +1,12 @@
 import { readFileSync, writeFileSync } from "fs";
+import bookList from "./bookList.json" with { type: "json" };
 
 const tagToDelete = "Audiobook";
+
+bookList.forEach((book) => {
+  for (let i = 0; i < book.genreTags.length; i += 1) {
+    if ((book.genreTags[i] = tagToDelete)) {
+      book.genreTags = book.genreTags.splice(i, 1);
+    }
+  }
+});
