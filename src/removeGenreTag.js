2ce0e4eaac484804fbc1ffa3cc1b26f54c14b22d
Greg Schneider <greg53x@msn.com>
2025-05-23 17:53:10 -0600

    swap to filter() to remove selected tags

--- src/removeGenreTag.js
+++ src/removeGenreTag.js
@@ -4,10 +4,8 @@ import bookList from "./bookList.json" with { type: "json" };
 const tagToDelete = "Audiobook";
 
 bookList.forEach((book) => {
-  for (let i = 0; i < book.genreTags.length; i += 1) {
-    if (book.genreTags[i] == tagToDelete) {
-      book.genreTags.splice(i, 1);
-    }
+  if (Array.isArray(book.genreTags)) {
+    book.genreTags = book.genreTags.filter((tag) => tag !== tagToDelete);
   }
 });
 
