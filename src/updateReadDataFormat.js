9b47ec8d82f7886b328a1a1eedde04a11bdc76fd
Greg Schneider <greg53x@msn.com>
2025-05-25 23:17:46 -0600

    check booklist for readingData properties

--- src/updateReadDataFormat.js
+++ src/updateReadDataFormat.js
@@ -1,6 +1,12 @@
 import { writeFileSync } from "fs";
 import bookList from "./bookList.json" with { type: "json" };
 
-bookList.forEach((book) => {});
+let i = 1;
+bookList.forEach((book) => {
+  if (!Object.hasOwn(book, "readingData")) {
+    console.log("no readingData", i);
+    i++;
+  }
+});
 
-writeFileSync("src/testUpdate.json", JSON.stringify(bookList, null, 2));
+// writeFileSync("src/testUpdate.json", JSON.stringify(bookList, null, 2));
