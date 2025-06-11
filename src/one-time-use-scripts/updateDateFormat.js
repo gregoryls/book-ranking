55536b6825ed4c57c8833e179475cd53bf8613ea
Greg Schneider <greg53x@msn.com>
2025-06-10 22:55:33 -0600

    change date read to iso 8601

--- src/one-time-use-scripts/updateDateFormat.js
+++ src/one-time-use-scripts/updateDateFormat.js
@@ -3,6 +3,9 @@ import bookList from "../bookList.json" with { type: "json" };
 
 bookList.forEach((book) => {
   if (book["Date Read"]) {
+    const [y1, m1, d1] = book["Date Read"].split("/");
+    // convert to iso 8601 format
+    book["Date Read"] = `${y1}-${m1}-${d1}`;
   }
 });
 
