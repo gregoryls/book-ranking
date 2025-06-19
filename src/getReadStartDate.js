05ab40d3c6b4d4cecbb89c78a8a071b95270e4bb
Greg Schneider <greg53x@msn.com>
2025-06-18 22:21:55 -0600

    fix already processed check to use correct property

--- src/getReadStartDate.js
+++ src/getReadStartDate.js
@@ -16,7 +16,7 @@ const main = async () => {
   for (const book of bookList) {
     if (!book["Date Read"]) continue;
     //check for YYYY-MM-DD format if a book has already been processed
-    // if (/^\d{4}-\d{2}-\d{2}$/.test(book["Date Read"])) continue;
+    if (/^\d{4}-\d{2}-\d{2}$/.test(book.readingData.started)) continue;
 
     let retries = 0;
     while (retries < maxRetries) {
