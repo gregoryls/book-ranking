e7c23bdd3c57412e306d3b84e5919a4a4f59ef3a
Greg Schneider <greg53x@msn.com>
2025-06-20 18:51:27 -0600

    modify script to include readingData finish date

--- src/one-time-use-scripts/updateDateFormat.js
+++ src/one-time-use-scripts/updateDateFormat.js
@@ -2,15 +2,20 @@ import { writeFileSync } from "fs";
 import bookList from "../bookList.json" with { type: "json" };
 
 bookList.forEach((book) => {
-  if (book["Date Read"]) {
-    const [y, m, d] = book["Date Read"].split("/");
+  // if (book["Date Read"]) {
+  //   const [y, m, d] = book["Date Read"].split("/");
+  //   // convert to iso 8601 format
+  //   book["Date Read"] = `${y}-${m}-${d}`;
+  // }
+  // if (book["Date Added"]) {
+  //   const [y, m, d] = book["Date Added"].split("/");
+  //   // convert to iso 8601 format
+  //   book["Date Added"] = `${y}-${m}-${d}`;
+  // }
+  if (book.readingData[0].finished) {
+    const [y, m, d] = book.readingData[0].finished.split("/");
     // convert to iso 8601 format
-    book["Date Read"] = `${y}-${m}-${d}`;
-  }
-  if (book["Date Added"]) {
-    const [y, m, d] = book["Date Added"].split("/");
-    // convert to iso 8601 format
-    book["Date Added"] = `${y}-${m}-${d}`;
+    book.readingData[0].finished = `${y}-${m}-${d}`;
   }
 });
 
