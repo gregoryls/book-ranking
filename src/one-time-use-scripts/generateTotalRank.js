69cfeea6bc6449def6b9fed88c89ffe6e3660713
Greg Schneider <greg53x@msn.com>
2025-06-26 15:20:13 -0600

    update notes

--- src/one-time-use-scripts/generateTotalRank.js
+++ src/one-time-use-scripts/generateTotalRank.js
@@ -3,5 +3,5 @@ import bookList from "../bookList.json" with { type: "json" };
 
 for (const book of bookList) {
 }
-
+// manually fix unread mistakes, abercrombie 2017?
 writeFileSync("src/testTotalRanks.json", JSON.stringify(bookList, null, 2));
