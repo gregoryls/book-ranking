1290e102f364832a884f14a56551258d5e8502f5
Greg Schneider <greg53x@msn.com>
2025-06-26 14:58:45 -0600

    finished splitting script

--- src/one-time-use-scripts/splitReadUnread.js
+++ src/one-time-use-scripts/splitReadUnread.js
@@ -1,7 +1,15 @@
 import { writeFileSync } from "fs";
 import bookList from "../bookList.json" with { type: "json" };
 
+const readList = [];
+const unreadList = [];
 for (const book of bookList) {
+  if (book.readingData[0].finished) {
+    readList.push(book);
+  } else {
+    unreadList.push(book);
+  }
 }
 
-writeFileSync("src/testTotalRanks.json", JSON.stringify(bookList, null, 2));
+writeFileSync("src/unreadList.json", JSON.stringify(unreadList, null, 2));
+writeFileSync("src/readList.json", JSON.stringify(readList, null, 2));
