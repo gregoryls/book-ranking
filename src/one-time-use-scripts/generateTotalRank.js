7cd342432640c3c995f5555d77ba9c0010224328
Greg Schneider <greg53x@msn.com>
2025-06-28 15:34:48 -0600

    assign total ranks based on rating buckets

--- src/one-time-use-scripts/generateTotalRank.js
+++ src/one-time-use-scripts/generateTotalRank.js
@@ -1,15 +1,32 @@
 import { writeFileSync } from "fs";
 import readList from "../readList.json" with { type: "json" };
 
-let count1 = 0,
-  count2 = 0,
-  count3 = 0,
-  count4 = 0,
-  count5 = 0;
+let count1 = 1,
+  count2 = 1,
+  count3 = 1,
+  count4 = 1,
+  count5 = 1;
 for (const book of readList) {
+  if (book.readingData.at(-1).rating === 1) {
+    book.totalRanking = count1;
+    count1++;
+  }
+  if (book.readingData.at(-1).rating === 2) {
+    book.totalRanking = count2;
+    count2++;
+  }
+  if (book.readingData.at(-1).rating === 3) {
+    book.totalRanking = count3;
+    count3++;
+  }
   if (book.readingData.at(-1).rating === 4) {
-    console.log(4);
+    book.totalRanking = count4;
+    count4++;
+  }
+  if (book.readingData.at(-1).rating === 5) {
+    book.totalRanking = count5;
+    count5++;
   }
 }
 
-// writeFileSync("src/testTotalRanks.json", JSON.stringify(bookList, null, 2));
+writeFileSync("src/testTotalRanks.json", JSON.stringify(bookList, null, 2));
