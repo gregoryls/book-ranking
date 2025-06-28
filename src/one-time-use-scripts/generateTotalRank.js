dacdc1e3186324f3dc51ecda1993a3afeabc5c8d
Greg Schneider <greg53x@msn.com>
2025-06-28 15:40:02 -0600

    fix readList writing typo

--- src/one-time-use-scripts/generateTotalRank.js
+++ src/one-time-use-scripts/generateTotalRank.js
@@ -29,4 +29,4 @@ for (const book of readList) {
   }
 }
 
-writeFileSync("src/testTotalRanks.json", JSON.stringify(bookList, null, 2));
+writeFileSync("src/testTotalRanks.json", JSON.stringify(readList, null, 2));
