64ae3e1c0c0cf5223b10615f9d3ffe11b70a16c8
Greg Schneider <greg53x@msn.com>
2025-07-03 14:49:13 -0600

    switch from bookList to readList

--- src/index.js
+++ src/index.js
@@ -21,35 +21,7 @@ import * as utils from "./utils.js";
 // placeholder cover when one doesn't exist
 // remove audiobook from genres
 
-const readBooks = [];
-const rating1Books = [];
-const rating2Books = [];
-const rating3Books = [];
-const rating4Books = [];
-const rating5Books = [];
-
-for (let i = 0; i < bookList.length; i += 1) {
-  if (bookList[i]["Date Read"]) {
-    readBooks.push(bookList[i]);
-  }
-  if (bookList[i]["My Rating"] === 1) {
-    rating1Books.push(bookList[i]);
-  }
-  if (bookList[i]["My Rating"] === 2) {
-    rating2Books.push(bookList[i]);
-  }
-  if (bookList[i]["My Rating"] === 3) {
-    rating3Books.push(bookList[i]);
-  }
-  if (bookList[i]["My Rating"] === 4) {
-    rating4Books.push(bookList[i]);
-  }
-  if (bookList[i]["My Rating"] === 5) {
-    rating5Books.push(bookList[i]);
-  }
-}
-
-utils.renderBookList(readBooks);
+utils.renderBookList(readList);
 
 // utils.dragAndDrop();
 const bookListRender = document.getElementById("bookWrap");
--- src/one-time-use-scripts/sortByTotalRank.js
+++ src/one-time-use-scripts/sortByTotalRank.js
@@ -1,5 +1,8 @@
-import { writeFileSync } from "fs";
+import { readlink, writeFileSync } from "fs";
 
 import readList from "../readList.json" with { type: "json" };
 
 const tempList = [];
+
+let i = 0;
+console.log(readList.length);
